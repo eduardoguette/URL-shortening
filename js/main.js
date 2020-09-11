2086880615dd35a104f5d611cd719a785b9ff476
Eduardo Guette <eduardoguette@github.com>
2020-09-11 23:18:48 +0200

    se agrego al LS

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const $btn = document.getElementById("shortern__btn");
-const $form = document.querySelector("form");
-const mensajeError = document.querySelector(".error-input");
-const shorterns = document.querySelector(".shorterns-urls__container");
+const $btn = document.getElementById('shortern__btn')
+const $form = document.querySelector('form')
+const mensajeError = document.querySelector('.error-input')
+const shorterns = document.querySelector('.shorterns-urls__container')
 
-$form.addEventListener("submit", (e) => {
-  let inputForm = document.getElementById("shortern__input").value.trim();
-  e.preventDefault();
-  let newShorten = "";
-  if (inputForm === "") {
-    mensajeError.style.display = "Block";
+$form.addEventListener('submit', (e) => {
+  let inputForm = document.getElementById('shortern__input').value.trim()
+  e.preventDefault()
+  let newShorten = ''
+  if (inputForm === '') {
+    mensajeError.style.display = 'Block'
   } else {
     getUrl(inputForm).then((data) => {
-      $input = "";
-      newShorten = data[0];
-      let urlShort = "https://rel.ink/" + newShorten.hashid;
+      $input = ''
+      newShorten = data[0]
+      let urlShort = 'https://rel.ink/' + newShorten.hashid
       if (inputForm.length > 32) {
-        var url_cor = newShorten.url.slice(0, 32) + "...";
+        var url_cor = newShorten.url.slice(0, 32) + '...'
       } else {
-        url_cor = inputForm;
+        url_cor = inputForm
       }
-      let divContainer = document.createElement("div");
-      divContainer.className = "shorterns-urls__card animate__animated animate__slideInLeft";
+      let divContainer = document.createElement('div')
+      divContainer.className = 'shorterns-urls__card animate__animated animate__slideInLeft'
       divContainer.innerHTML = `
           <div class="container-x">
           <a href=${newShorten.url} class="shorterns-urls__url">${url_cor}</a>
@@ -29,31 +29,31 @@ $form.addEventListener("submit", (e) => {
           <hr class="separador" />
           <a href=${urlShort} class="short-url">${urlShort}</a>
           <button class="btn-copy" data-clipboard-text=${urlShort}>Copy</button>
-          `;
-      document.querySelector(".shorterns-urls__container").appendChild(divContainer);
+          `
+      document.querySelector('.shorterns-urls__container').appendChild(divContainer)
 
-      if (!localStorage.getItem("arraysShorten")) {
-        localStorage.setItem("arraysShorten", JSON.stringify([newShorten]));
+      if (!localStorage.getItem('arraysShorten')) {
+        localStorage.setItem('arraysShorten', JSON.stringify([newShorten]))
       } else {
-        let tmpLS = JSON.parse(localStorage.getItem("arraysShorten"));
-        tmpLS = [...tmpLS, newShorten];
-        localStorage.setItem("arraysShorten", JSON.stringify(tmpLS));
+        let tmpLS = JSON.parse(localStorage.getItem('arraysShorten'))
+        tmpLS = [...tmpLS, newShorten]
+        localStorage.setItem('arraysShorten', JSON.stringify(tmpLS))
       }
-    });
+    })
   }
-});
+})
 
-if (localStorage.getItem("arraysShorten")) {
-  render();
+if (localStorage.getItem('arraysShorten')) {
+  render()
 }
 
 function render() {
-  let arrayRender = JSON.parse(localStorage.getItem("arraysShorten"));
+  let arrayRender = JSON.parse(localStorage.getItem('arraysShorten'))
   arrayRender.map((elem, i) => {
-    let urlShort = "https://rel.ink/" + elem.hashid;
-    let divContainer = document.createElement("div");
-    divContainer.className = "shorterns-urls__card animate__animated animate__slideInLeft";
-    var url_cor = elem.url.slice(0, 32) + "...";
+    let urlShort = 'https://rel.ink/' + elem.hashid
+    let divContainer = document.createElement('div')
+    divContainer.className = 'shorterns-urls__card animate__animated animate__slideInLeft'
+    var url_cor = elem.url.slice(0, 32) + '...'
     if (elem.url.length > 32) {
       divContainer.innerHTML = `
       <div class="container-x">
@@ -63,7 +63,7 @@ function render() {
       <hr class="separador" />
       <a href=${urlShort} class="short-url">${urlShort}</a>
       <button class="btn-copy" data-clipboard-text=${urlShort}>Copy</button>
-      `;
+      `
     } else {
       divContainer.innerHTML = `
       <div class="container-x">
@@ -73,55 +73,60 @@ function render() {
       <hr class="separador" />
       <a href=${urlShort} class="short-url">${urlShort}</a>
       <button class='btn-copy' data-clipboard-text=${urlShort}>Copy</button>
-      `;
+      `
     }
-    document.querySelector(".shorterns-urls__container").appendChild(divContainer);
-  });
+    document.querySelector('.shorterns-urls__container').appendChild(divContainer)
+  })
 }
 
 // fetch services
 async function getUrl(url) {
-  let response = [];
-  await fetch("https://rel.ink/api/links/", {
+  let response = []
+  await fetch('https://rel.ink/api/links/', {
     headers: {
-      accept: "application/json, text/plain, */*",
-      "accept-language": "es-US,es;q=0.9,en-US;q=0.8,en;q=0.7,es-419;q=0.6",
-      "content-type": "application/json;charset=UTF-8",
+      accept: 'application/json, text/plain, */*',
+      'accept-language': 'es-US,es;q=0.9,en-US;q=0.8,en;q=0.7,es-419;q=0.6',
+      'content-type': 'application/json;charset=UTF-8',
     },
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       url: url,
     }),
   })
     .then((resp) => resp.json())
-    .then((data) => (response = [...response, data]));
-  return response;
+    .then((data) => (response = [...response, data]))
+  return response
 }
 
 // Función para copiar url
 function CopyClipboard() {
-  document.querySelector(".shorterns-urls__container").addEventListener("click", (e) => {
-    e.stopPropagation();
-    var btns = document.querySelectorAll(".btn-copy");
-    var clipboard = new ClipboardJS(btns);
-    clipboard.on("success", function (e) {
-      e.trigger.innerHTML = "Copied!";
-      e.trigger.style.backgroundColor = "#3b3054";
+  document.querySelector('.shorterns-urls__container').addEventListener('click', (e) => {
+    e.stopPropagation()
+    var btns = document.querySelectorAll('.btn-copy')
+    var clipboard = new ClipboardJS(btns)
+    clipboard.on('success', function (e) {
+      e.trigger.innerHTML = 'Copied!'
+      e.trigger.style.backgroundColor = '#3b3054'
       setTimeout(() => {
-        e.trigger.innerHTML = "Copy";
-        e.trigger.style.backgroundColor = "#2acfcf";
-      }, 500);
-    });
-    clipboard.on("error", function (e) {
-      console.log(e);
-    });
-  });
+        e.trigger.innerHTML = 'Copy'
+        e.trigger.style.backgroundColor = '#2acfcf'
+      }, 500)
+    })
+    clipboard.on('error', function (e) {
+      console.log(e)
+    })
+  })
 }
-CopyClipboard();
+CopyClipboard()
 
-document.querySelector(".shorterns-urls__container").addEventListener("click", (e) => {
-  if (e.target.tagName === "IMG") {
-    e.target.parentElement.parentElement.remove();
-    
+document.querySelector('.shorterns-urls__container').addEventListener('click', (e) => {
+  if (e.target.tagName === 'IMG') {
+    let urlId = e.target.parentElement.parentElement.querySelector('.short-url').textContent.split('/').pop().trim()
+    let LS = JSON.parse(localStorage.getItem('arraysShorten'))
+    let newArray = LS.filter(({ hashid }, index) => hashid.indexOf(urlId) !== index)
+    console.log(newArray)
+    console.log(urlId)
+    localStorage.setItem('arraysShorten', JSON.stringify(newArray))
+    //  e.target.parentElement.parentElement.remove()
   }
-});
+})
